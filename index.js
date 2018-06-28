1f6c7446b6c943a179390e57cfd085531fce3972
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2018-06-28 18:13:38 +0100

    First test payments using stripe.js

--- index.js
+++ index.js
@@ -4,18 +4,53 @@ const {
   SERVER_HOST,
 } = process.env;
 
+const CURRENCY = 'gbp';
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const stripe = require('stripe')(STRIPE_API_KEY);
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.listen(SERVER_PORT, SERVER_HOST);
 
-app.use(express.static('public'));
+app.post('/api/payement', (req, res) => {
+  const { body: { token } } = req;
 
-const payement = (req, res) => {
-  res.send('hello world');
-};
+  // Create a Charge:
+  stripe.charges.create({
+    amount: 10000,
+    currency: CURRENCY,
+    source: 'tok_visa',
+    transfer_group: "{ORDER10}",
+  }).then(function(charge) {
+    console.log(charge)
+    // asynchronously called
+  });
 
-app.get('/api/payement', payement);
+  // // Create a Transfer to the connected account (later):
+  // stripe.transfers.create({
+  //   amount: 7000,
+  //   currency: CURRENCY,
+  //   destination: "{CONNECTED_STRIPE_ACCOUNT_ID}",
+  //   transfer_group: "{ORDER10}",
+  // }).then(function(transfer) {
+  //   // asynchronously called
+  // });
+  //
+  // // Create a second Transfer to another connected account (later):
+  // stripe.transfers.create({
+  //   amount: 2000,
+  //   currency: CURRENCY,
+  //   destination: "{OTHER_CONNECTED_STRIPE_ACCOUNT_ID}",
+  //   transfer_group: "{ORDER10}",
+  // }).then(function(second_transfer) {
+  //   // asynchronously called
+  // });
+
+  res.status(200).jsonp(token);
+});
 
